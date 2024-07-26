24f82b6ebe144ed3dc6873d3ece02236aac9a218
saadzafar0 <msaadzafar0@gmail.com>
2024-07-26 18:34:50 +0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,37 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import AllPosts from "../pages/AllPosts";
+import EditPost from "../pages/EditPost";
+import AddPost from "../pages/AddPost";
+import Signup from "./Signup";
+
+export {
+  Signup,
+  AddPost,
+  EditPost,
+  AllPosts,
+  Select,
+  Input,
+  Button,
+  AuthLayout,
+  PostCard,
+  PostForm,
+  Login,
+  RTE,
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+};
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+
+    // post:postSlice
+    //TODO: add more slices here for posts
+  },
+});
+
+export default store;
